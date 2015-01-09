4ab6b0f1bee69673f1af4b928cc9c68f96f17f7e
Doug-Binder <doug@fintech.io>
2015-01-08 22:46:04 -0500

    am

--- app/views.py
+++ app/views.py
@@ -35,9 +35,6 @@ class LandScapeView(ListView):
         context = super(LandScapeView, self).get_context_data(**kwargs)
         context['title'] = 'LANDSCAPES'
         context["prefix"] = 'landscape'
-        for scape in context['landscapes']:
-            scape.page = context['page_obj'].number
-            scape.save()
         return context
 
 class HardScapeView(ListView):
